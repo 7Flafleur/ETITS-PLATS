d341d866cd7fb4f1dd563db493c4d6de0e0cadca
7Flafleur <7nb@hotmail.fr>
2024-02-26 17:20:55 +0100

    filter searchbar

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,52 @@
 displayCards(recipes);
 
 //glabaol vriable,updated after each sarch function call
- let visibleRecipesList=document.querySelectorAll(".card [data-visble='true']")
+ let visibleRecipesList=document.querySelectorAll(".card[data-visible='true' ]")
 
  let visibleRecipesArray=Array.from(visibleRecipesList);
 
- let hiddenRecipesList=document.querySelectorAll(".card [data-visible='false']")
+ let hiddenRecipesList=document.querySelectorAll(".card[data-visible='false']")
 
  let hiddenRecipesArray=[...hiddenRecipesList] //create Array ,spread operator
 
+ const recipesection=document.querySelector(".card_container")
 
  const loupe=document.getElementById("loupe");
  const queryInput=document.getElementById("queryInput")
 
 
- queryInput.addEventListener("input", (event)=>{
-     let query= event.target.value;
-     
-     if(query.length>=3){
-      
-     console.log(query) 
-     }
+ queryInput.addEventListener("input", (event) => {
+  let query = event.target.value.toUpperCase();
 
- })
+  if (query.length >= 3) {
+      console.log(query);
+      visibleRecipesList.forEach((recipe) => {
+          let titre = recipe.querySelector("h2").textContent.toUpperCase();
+          let recette = recipe.querySelector(".card_content-recette").textContent.toUpperCase();
+          let ingredients = recipe.querySelector(".card_content-ingredients").textContent.toUpperCase();
+          if (!titre.includes(query) && !recette.includes(query) && !ingredients.includes(query)) {
+              recipe.dataset.visible = "false";
+              console.log("not included");
+          }
+          else{
+            recipe.dataset.visible="true";
+          }
+          visibleRecipesList=document.querySelectorAll(".card[data-visible='true' ]")
+          hiddenRecipesList=document.querySelectorAll(".card[data-visible='false']")
+
+      });
+      if(visibleRecipesList.length==0){
+        console.log("Aucune recette trouvée!")
+        
+      }
+  }  
+  else {
+    if(hiddenRecipesList.length!=0)
+    hiddenRecipesList.forEach((recipe=>{
+  recipe.dataset.visible="true"}
+  ))
+  }
+});
 
 
 
