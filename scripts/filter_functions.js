9bd8b721ae9de35c1c1ed741a1d0cf9ec4fb4671
7Flafleur <7nb@hotmail.fr>
2024-02-27 11:36:35 +0100

    resultmsg center

--- scripts/filter_functions.js
+++ scripts/filter_functions.js
@@ -1,127 +1,116 @@
 //VARIABLES
 
-
 function filterDropDown(button, dropdown, input, items) {
-    //create dropdown items from list of items
-    items.forEach((item) =>{
-        let dropdown_item = document.createElement('li');
-        dropdown_item.setAttribute("data-visible","true");
-        dropdown_item.setAttribute('class', 'dropdown-item');
-        dropdown_item.innerHTML = item;
-        dropdown.appendChild(dropdown_item);
-    }) 
-    //hide the dropdown list
-    dropdown.style.display = 'none';
+  //create dropdown items from list of items
+  items.forEach((item) => {
+    let dropdown_item = document.createElement("li");
+    dropdown_item.setAttribute("data-visible", "true");
+    dropdown_item.setAttribute("class", "dropdown-item");
+    dropdown_item.innerHTML = item;
+    dropdown.appendChild(dropdown_item);
+  });
+  //hide the dropdown list
+  dropdown.style.display = "none";
 
-    //make the button toggle the display of dropdown
-    button.addEventListener('click', function () {
-        if (dropdown.style.display == 'none')
-            dropdown.style.display = 'block';
-        else
-            dropdown.style.display = 'none';
-    });
+  //make the button toggle the display of dropdown
+  button.addEventListener("click", function () {
+    if (dropdown.style.display == "none") dropdown.style.display = "block";
+    else dropdown.style.display = "none";
+  });
 
-    //filter function
+  //filter function
 
-    input.addEventListener('input', function () {
-        let dropdown_items = dropdown.querySelectorAll('.dropdown-item');
-        if(input.value.length>=3)
-    {        if (!dropdown_items)
-            return false;
-        for (let i=0; i<dropdown_items.length; i++) {
-            if (dropdown_items[i].innerHTML.toUpperCase().includes(input.value.toUpperCase()))
-                dropdown_items[i].style.display = 'block';
-            else
-                dropdown_items[i].style.display = 'none';
-        }}
-        updateRecipeCount();
-    });
+  input.addEventListener("input", function () {
+    let dropdown_items = dropdown.querySelectorAll(".dropdown-item");
+    if (input.value.length >= 3) {
+      if (!dropdown_items) return false;
+      for (let i = 0; i < dropdown_items.length; i++) {
+        if (
+          dropdown_items[i].innerHTML
+            .toUpperCase()
+            .includes(input.value.toUpperCase())
+        )
+          dropdown_items[i].style.display = "block";
+        else dropdown_items[i].style.display = "none";
+      }
+    }
+    updateRecipeCount();
+  });
 }
-
-
-
-
 
 // //on click on item, close dropdown, create tag
- function createTag(item,tag){
-    let parent=item.parentNode
-     parent.style.display="none";  
-     const searchtag =document.createElement("span")
-        searchtag.setAttribute("class", "searchtag")
-        searchtag.textContent=tag
-    const tagsection=document.querySelector(".tag_section")    
-    tagsection.append(searchtag);
-
+function createTag(item, tag) {
+  let parent = item.parentNode;
+  parent.style.display = "none";
+  const searchtag = document.createElement("span");
+  searchtag.setAttribute("class", "searchtag");
+  searchtag.textContent = tag;
+  const tagsection = document.querySelector(".tag_section");
+  tagsection.append(searchtag);
 }
 
+function findCards(query) {
+  console.log(query);
+  let visibleRecipesList = document.querySelectorAll(
+    ".card[data-visible='true' ]"
+  );
+  visibleRecipesList.forEach((recipe) => {
+    let titre = recipe.querySelector("h2").textContent.toUpperCase();
+    let recette = recipe
+      .querySelector(".card_content-recette")
+      .textContent.toUpperCase();
+    let ingredients = recipe
+      .querySelector(".card_content-ingredients")
+      .textContent.toUpperCase();
+    if (
+      !titre.includes(query) &&
+      !recette.includes(query) &&
+      !ingredients.includes(query)
+    ) {
+      recipe.dataset.visible = "false";
+      console.log("not included");
+    } else {
+      recipe.dataset.visible = "true";
+    }
+    visibleRecipesList = document.querySelectorAll(
+      ".card[data-visible='true' ]"
+    );
+    updateRecipeCount();
+  });
+  if (visibleRecipesList.length == 0) {
+    displayNoResults();
+  }
 
-function findCards(query){
-    if (query.length >= 3) {
-        console.log(query);
-        let visibleRecipesList=document.querySelectorAll(".card[data-visible='true' ]")      
-        visibleRecipesList.forEach((recipe) => {
-            let titre = recipe.querySelector("h2").textContent.toUpperCase();
-            let recette = recipe.querySelector(".card_content-recette").textContent.toUpperCase();
-            let ingredients = recipe.querySelector(".card_content-ingredients").textContent.toUpperCase();
-            if (!titre.includes(query) && !recette.includes(query) && !ingredients.includes(query)) {
-                recipe.dataset.visible = "false";
-                console.log("not included");
-            }
-            else{
-              recipe.dataset.visible="true";
-            }
-            visibleRecipesList=document.querySelectorAll(".card[data-visible='true' ]")
-        updateRecipeCount()
-    
-  
-        });
-        if(visibleRecipesList.length==0){
-        displayNoResults();
-        
-          
-        }
-    } 
-    else if (query==""){
-      resetCards()
-      updateRecipeCount()
-    } 
-  
+  if (query == "") {
+    resetCards();
+    updateRecipeCount();
+  }
 }
 
-
-
-
-
-function displayNoResults(){
-    const resultmsg=document.getElementById("resultmsg")
-    resultmsg.textContent="Aucune recette trouvée";
+function displayNoResults() {
+  const resultmsg = document.getElementById("resultmsg");
+  resultmsg.textContent = "Aucune recette trouvée";
 }
-
 
 function updateRecipeCount() {
-    let visibleRecipesList=document.querySelectorAll(".card[data-visible='true']")
-    let nb = visibleRecipesList.length;
-    nb_recettes.textContent = nb + " recettes";
+  let visibleRecipesList = document.querySelectorAll(
+    ".card[data-visible='true']"
+  );
+  let nb = visibleRecipesList.length;
+  nb_recettes.textContent = nb + " recettes";
 }
 
 // Call this function every time `visibleRecipesList` is updated
 
-
-function resetCards(){
-    //cards.removeAttribute
-    const cards=document.querySelectorAll(".card")
-    cards.forEach((card)=>{
-        card.setAttribute("data-visible","true")
-    })
-    queryInput.value="";
-    resultmsg.textContent=""
-    updateRecipeCount()
-   
-
+function resetCards() {
+  //cards.removeAttribute
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.setAttribute("data-visible", "true");
+  });
+  queryInput.value = "";
+  resultmsg.textContent = "";
+  updateRecipeCount();
 }
 
-
-
-
 //call filterDropDown function
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,29 @@
-
 //glabaol vriable,updated after each sarch function call
 
-const nb_recettes=document.querySelector(".nb_recipes")
+const nb_recettes = document.querySelector(".nb_recipes");
 
- const recipesection=document.querySelector(".card_container")
+const recipesection = document.querySelector(".card_container");
 
- const loupe=document.getElementById("loupe");
- const queryInput=document.getElementById("queryInput")
+const loupe = document.getElementById("loupe");
+const queryInput = document.getElementById("queryInput");
 
+///SELECT DROPDOWN////////////
+const ingredientsButton = document.getElementById("toggle1");
+const ingredientsSelect = document.getElementById("dropdown1");
+const ingredientsInput = document.getElementById("inputIn");
 
+const appareilsButton = document.getElementById("toggle2");
+const appareilsSelect = document.getElementById("dropdown2");
+const appareilsInput = document.getElementById("inputA");
 
- ///SELECT DROPDOWN////////////
- const ingredientsButton=document.getElementById('toggle1')
- const ingredientsSelect=  document.getElementById('dropdown1')
- const ingredientsInput=document.getElementById('inputIn')
+const ustensileButton = document.getElementById("toggle3");
+const ustensileSelect = document.getElementById("dropdown3");
+const ustensileInput = document.getElementById("inputU");
 
- const appareilsButton=document.getElementById('toggle2')
- const appareilsSelect=document.getElementById('dropdown2')
- const appareilsInput=    document.getElementById('inputA')
-
- const ustensileButton=  document.getElementById('toggle3')
- const ustensileSelect=  document.getElementById('dropdown3')
- const ustensileInput=  document.getElementById('inputU')
-
-
-
- //////////////MAIN CODE///////////////////////
+//////////////MAIN CODE///////////////////////
 displayCards(recipes);
 updateRecipeCount();
-queryInput.value="";
+queryInput.value = "";
 
 /////////////EVENT LISTENER FOR SEARCHBAR ///////////////////////
 //  queryInput.addEventListener("input", (event) => {
@@ -37,83 +32,68 @@ queryInput.value="";
 
 // });
 
-loupe.addEventListener("click", (event)=>
-{ event.preventDefault();
-  let query=queryInput.value.toUpperCase();
-  findCards(query);
-  updateRecipeCount()
-})
+loupe.addEventListener("click", (event) => {
+  event.preventDefault();
+  let query = queryInput.value.toUpperCase();
+  if (query.length >= 3) {
+    findCards(query);
+  }
 
-
+  updateRecipeCount();
+});
 
 ////////CREATE SELECT MENUS//////////////////////
 filterDropDown(
-  ingredientsButton, // 
-ingredientsSelect,
-ingredientsInput,
+  ingredientsButton, //
+  ingredientsSelect,
+  ingredientsInput,
   IngredientsArray
-)
+);
 
-
-filterDropDown(
-
-  appareilsButton,
-  appareilsSelect,
-  appareilsInput,
-  AppareilsList
-)
-
+filterDropDown(appareilsButton, appareilsSelect, appareilsInput, AppareilsList);
 
 filterDropDown(
-ustensileButton,
-ustensileSelect,
-ustensileInput,
+  ustensileButton,
+  ustensileSelect,
+  ustensileInput,
   UstensilesList
-)
-
+);
 
 //apply cklickfunction to visible elements in dropdown
-let visibleSelectItems = document.querySelectorAll('.dropdown-item[data-visible]');
+let visibleSelectItems = document.querySelectorAll(
+  ".dropdown-item[data-visible]"
+);
 
 //get clicked item
-let dropdownitems=document.querySelectorAll("dropdown-item")
-
+let dropdownitems = document.querySelectorAll("dropdown-item");
 
 // create tag for chosen term
-visibleSelectItems.forEach(item => {
-    item.addEventListener("click", event => {
-      let tag=event.currentTarget.textContent
-      // console.log(tag);
-      
-      // Add more functions here
-      createTag(item,tag);
-  //remove tag on click
-  let searchtags=document.querySelectorAll(".searchtag")
+visibleSelectItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let tag = event.currentTarget.textContent;
+    // console.log(tag);
 
-  console.log(searchtags)
+    // Add more functions here
+    createTag(item, tag);
+    //remove tag on click
+    let searchtags = document.querySelectorAll(".searchtag");
 
-  //remove tag on click
-  searchtags.forEach(item => {
-    item.addEventListener("click", event => {
-      let term=event.currentTarget.textContent
-      console.log(term)
-      event.currentTarget.remove();
-      
+    console.log(searchtags);
 
-      
+    //remove tag on click
+    searchtags.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        let term = event.currentTarget.textContent;
+        console.log(term);
+        event.currentTarget.remove();
+      });
     });
+
+    //////////RUN SEARCH FUNCTION////////////////////
+    findCards(tag);
   });
+});
 
-  //////////RUN SEARCH FUNCTION////////////////////
-   findCards(tag)
-    
-    });
-  });
-
-  //////////RESET BUTTON FOR TESTING////////////////////
-  const reset=document.getElementById("reset")
-  reset.addEventListener("click", resetCards)
-
-
-
-
+//////////RESET BUTTON FOR TESTING////////////////////
+const reset = document.getElementById("reset");
+reset.addEventListener("click", resetCards);
